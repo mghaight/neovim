cbbb6d86b5752ca776ebba4f017cd42f7c069108
Mackenzie Haight <mackenzie.haight@gmail.com>
2025-10-15 11:49:54 -0400

    adding keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,3 +1,6 @@
+-- clear search highlighting
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
 -- Emacs (readline) keybindings to move in insert mode
 vim.keymap.set("i", "<C-n>", "<Down>")
 vim.keymap.set("i", "<C-p>", "<Up>")
@@ -13,3 +16,10 @@ vim.keymap.set("i", "<C-l>", "<C-o>zz")
 -- use C-j and C-k to expose (scroll) line below or above current screen
 vim.keymap.set("n", "<C-j>", "<C-e>")
 vim.keymap.set("n", "<C-k>", "<C-y>")
+
+-- taking back digraphs from my readline conflicts
+vim.keymap.set("i", "<C-q>", "<C-k>", { noremap = true, desc = "Insert digraph" })
+
+-- splitting windows
+vim.keymap.set("n", "<C-w>\\", ":vsplit<CR>")
+vim.keymap.set("n", "<C-w>-", ":split<CR>")
